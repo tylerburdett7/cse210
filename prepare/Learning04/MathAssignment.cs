9a28e5207c2b0873121cee71787ff578735ea54f
tylerburdett7 <bur23034@byui.edu>
2025-02-20 23:10:10 -0700

    completed learning 4

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,18 @@
+using System.Data;
+
+public class MathAssignment : Assignment {
+    private string _textbookSection;
+    private string _problems;
+
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems): base(studentName, topic) {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+    public string getHomeworkList() {
+        return $"Section: {_textbookSection} problem: {_problems}";
+    }
+
+
+
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
-    }
+        var Assignment = new Assignment("tyler", "science");
+        var summary =Assignment.GetSummary();
+        Console.WriteLine(summary);
+
+        var MathAssignment = new MathAssignment("tyboos", "math", "5.4", "45-50");
+        var homeworkList = MathAssignment.getHomeworkList();
+        Console.WriteLine(homeworkList);
+
+        var WritingAssignment = new WritingAssignment("tyler", "english", "I am only slightly confused");
+        var writingInformation = WritingAssignment.GetWritingInformation();
+        Console.WriteLine(writingInformation);
+    } 
 }
